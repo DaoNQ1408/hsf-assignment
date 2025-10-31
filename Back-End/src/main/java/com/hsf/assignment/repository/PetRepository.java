38dfefb6c42f21f2440d20650770832bdb8d2d05
Lifeboi_13 <duongnphse181794@fpt.edu.vn>
2025-11-01 02:13:12 +0700

    feat: crud pet

--- Back-End/src/main/java/com/hsf/assignment/repository/PetRepository.java
+++ Back-End/src/main/java/com/hsf/assignment/repository/PetRepository.java
@@ -1,9 +1,13 @@
 package com.hsf.assignment.repository;
 
+
 import com.hsf.assignment.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PetRepository extends JpaRepository<Pet,Long> {
+    List<Pet> findBySpecies(String species);
 }
